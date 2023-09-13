afd58a65bd7442ca85febbc556a3067ab95692cd
Rahmat Hidayat <yayatbx@gmail.com>
2023-09-13 22:49:39 +0800

    first push my react native app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={textTest.testStyle}>Hello World</Text>
     </View>
   );
 }
@@ -11,8 +11,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const textTest = StyleSheet.create({
+  testStyle: {
+    color: "#00941e",
+    fontWeight: "700",
   },
 });
